9e370f640e9f1219db0e4fc0e1f349bffd49430b
Aurélien Roux <aureroux13@me.com>
2017-01-16 20:27:18 +0000

    recursive functions

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,33 @@
 console.log("Hello World!");
 
+// function timing(){
+//     setTimeout(function(){
+//         console.log("Hello World Again!!");
+//     }, 3000);
+// }
+
+// timing();
+
+// function timing(){
+//     setInterval(function(){
+//         console.log("interval");
+//     }, 2000);
+// }
+
+// timing();
+
+
+
+var i = 0;
 function timing(){
-    setTimeout(function(){
-        console.log("Hello World Again!!");
-    }, 3000);
-};
+     
+     setTimeout(function(){
+         i++;
+         if( i < Infinity ){
+            console.log("test");
+            timing();
+         }
+     }, 2000);
+} 
 
 timing();
